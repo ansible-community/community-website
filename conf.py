b11bc6ba2a2fdd01e5a4335cc43338b8b62463dd
Don Naro <dnaro@redhat.com>
2023-04-04 21:31:14 +0100

    gh-pages deploy branch

--- conf.py
+++ conf.py
@@ -685,7 +685,7 @@ REDIRECTIONS = []
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
 GITHUB_SOURCE_BRANCH = 'main'
-GITHUB_DEPLOY_BRANCH = 'prod'
+GITHUB_DEPLOY_BRANCH = 'gh-pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
 GITHUB_REMOTE_NAME = 'origin'
