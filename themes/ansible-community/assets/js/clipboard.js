bf8353c43bc399d89e918a9fe56d501653f81fdf
Don Naro <dnaro@redhat.com>
2023-09-20 10:20:15 +0100

    issue #159 design feedback on fonts and banner
    - Use Red Hat display as the font family globally.
    - Adjust font weight on main banner content.
    - Add tooltips to install and get started buttons.
    - Move "Install by copying" text to tooltip.
    - Add popup to make it clear when text is copied.
    - Add right arrows to platform learn more in banner.
    - Use fa right arrow instead of circle right arrow.
    - Avoid increasing space between buttons when resizing page.
    
    Co-authored-by: Tina Yip <98424339+tiyiprh@users.noreply.github.com>

--- themes/ansible-community/assets/js/clipboard.js
+++ themes/ansible-community/assets/js/clipboard.js
@@ -4,6 +4,16 @@ clipboard.on('success', function (e) {
   console.info('Action:', e.action);
   console.info('Text:', e.text);
   console.info('Trigger:', e.trigger);
+
+  // Display the "Copied" popup text
+  var popup = document.getElementById('clipboard-popup');
+  popup.classList.remove('hidden');
+
+  // Hide after 2 seconds
+  setTimeout(function() {
+    popup.classList.add('hidden');
+  }, 2000);
+
 });
 
 clipboard.on('error', function (e) {
