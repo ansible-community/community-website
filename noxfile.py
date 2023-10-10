09fde1838c7e4a283703e88391e7bd7540b1a951
Don Naro <dnaro@redhat.com>
2023-10-10 08:55:43 +0100

    issue #215 spelling check fixes (#270)

--- noxfile.py
+++ noxfile.py
@@ -49,10 +49,10 @@ def spelling(session: nox.Session):
     """
     Spell check the website and blog
     """
-    session.install("codespell", "tomli")
+    session.install("codespell")
     session.run("codespell",
         *session.posargs,
-        "--toml", f"pyproject.toml"
+        "./",
         )
 
 @nox.session
